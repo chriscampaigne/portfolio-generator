0cb6b5a9420bbad25c2a4ed62c610221ffd621d0
chriscampaigne <campaignemc@gmail.com>
2022-07-26 11:29:50 -0400

    added html for projects

--- app.js
+++ app.js
@@ -136,17 +136,30 @@ if (!portfolioData.projects) {
   });
 };
 
-promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    console.log(portfolioData);
-  });
-// const generatePage = require('./src/page-template.js');
+// promptUser()
+  //.then(promptProject)
+  // .then(portfolioData => {
+    // const pageHTML = generatePage(portfolioData);
 
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
+  // });
+
+  const mockData = {
+    name: 'Lernantino',
+    github: 'lernantino',
+    projects: []
+  }
+ const generatePage = require('./src/page-template.js');
+
+ const pageHTML = generatePage(mockData);
 // const pageHTML = generatePage(name, github);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-  // if (err) throw err;
+fs.writeFile('./index.html', pageHTML, err => {
+   if (err) throw err;
 
-  // console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+  console.log('Portfolio complete! Check out index.html to see the output!');
+ });
